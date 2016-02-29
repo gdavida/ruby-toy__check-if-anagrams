fa14b4a08f0934905e0828da4e5d101ff766cee8
Davida Gaffney <gdavida@gmail.com>
2016-02-29 13:50:39 +0000

    Done w/Comments

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,16 +1,20 @@
 
 def anagrams?(str1, str2)
 	array_str1 = str1.split(//)
+	#split str1 into an array by character
 	check_str1 = array_str1.sort.join("")
-
+	#alphabetize the array, then join back into a string
 
 	array_str2 = str2.split(//)
+	#split str2 into an array by character
 	check_str2 = array_str2.sort.join("")
+	#alphabetize the array, then join back into a string
 
 	if check_str1 == check_str2
-		return true
+	#check if letters of each word, once sorted into alphabetical order, are equal to each other
+		puts "true"
 	else
-		return false
+		puts "false"
 	end
 end
 
