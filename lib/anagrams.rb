3b30ff539708b318139384b2111bed4bbdceadb3
Davida Gaffney <gdavida@gmail.com>
2016-02-29 13:49:23 +0000

    Done

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,2 +1,16 @@
+
 def anagrams?(str1, str2)
+	array_str1 = str1.split(//)
+	check_str1 = array_str1.sort.join("")
+
+
+	array_str2 = str2.split(//)
+	check_str2 = array_str2.sort.join("")
+
+	if check_str1 == check_str2
+		return true
+	else
+		return false
+	end
 end
+
